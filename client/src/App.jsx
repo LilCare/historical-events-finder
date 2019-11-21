2efb03bb6c4912512302f6802804b9a726df0fc5
Lily Carey <lilcare@Lilys-MacBook-Air.local>
2019-11-20 23:08:27 -0800

    Implement basic pagination feature that sets results on change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,26 @@
 import React, { useState } from 'react';
-import Search from './Search.jsx';
+import Search from './Search';
+import Paginate from './Paginate';
 
 function App() {
-  const [termState, setTerm] = useState('');
+  const [searchState, setSearch] = useState('');
   const [resultsState, setResults] = useState([]);
-
-  
+  const [pageCountState, setPageCountState] = useState(0);
 
   return (
     <div>
       <h1 id="title">
         Historical Fact Finder
       </h1>
-
+      <Search
+        searchState={searchState}
+        setSearch={setSearch}
+        setResults={setResults}
+        setPageCountState={setPageCountState}
+      />
+      <Paginate
+        pageCountState={pageCountState}
+      />
     </div>
   );
 }
--- client/src/Search.jsx
+++ client/src/Search.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
-function Search({ searchState, setSearch, setResults }) {
+function Search({ searchState, setSearch, setResults, setPageCountState }) {
+
   function searchDB(searchTerm) {
-    // Todo: Create function to query json DB and use setResults
+    axios.get(`/events?q=${searchTerm}&_page=1&_limit=10`)
+      .then((results) => {
+        setResults(results.data);
+        const linksSplit = results.headers.link.split('page=');
+        // last page will in the 3rd index in the first spot.
+        const lastPageNumber = parseInt(linksSplit[3][0], 10);
+        setPageCountState(lastPageNumber);
+      })
+      .catch((err)=> console.log(err));
   }
 
   return (
-    <form onSubmit={() => searchDB(searchState)}>
+    <form onSubmit={(e) => { e.preventDefault(); searchDB(searchState); }}>
       <label htmlFor="search">
         Search a historical fact:
-        <input type="text" value={searchState} onChange={(e) => setSearch(e.target.value)} id="search" />
+        <input
+          type="text"
+          id="search"
+          value={searchState}
+          onChange={(e) => { setSearch(e.target.value); }}
+        />
       </label>
+      <input type="submit" value="Submit" />
     </form>
   );
 }
@@ -22,4 +38,5 @@ Search.propTypes = {
   searchState: PropTypes.string.isRequired,
   setSearch: PropTypes.func.isRequired,
   setResults: PropTypes.func.isRequired,
+  setPageCountState: PropTypes.func.isRequired,
 };
